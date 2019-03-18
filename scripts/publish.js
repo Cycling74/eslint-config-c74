808dc41bfc108e2a03a04e699ab6fe8abd992fd5
Ben Bracken <ben@cycling74.com>
2019-03-18 15:26:40 -0700

    npm publish script

---
+++ scripts/publish.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const { join } = require("path");
+const { execSync } = require("child_process");
+const { executeMain, REPO_DIR, runAsync } = require("./helpers");
+
+executeMain(async () => {
+
+
+	const changes = execSync("git status -s").toString();
+	if (changes && changes.length) {
+		console.error("Git Directory not clean. Please make sure you are working from a clean HEAD");
+		process.exit(1);
+		return;
+	}
+
+	await runAsync("yarn", ["run", "test"]);
+	await runAsync("yarn", ["run", "version"]);
+
+	const packageInfo = require("../package.json");
+
+	await runAsync("git", [
+		"add",
+		"./package.json",
+		"./packages/eslint-config-c74/package.json",
+		"./packages/eslint-config-c74-base/package.json",
+		"./packages/eslint-config-c74-ts/package.json",
+	]);
+
+	await runAsync("git", [
+		"commit",
+		"-m",
+		`"Set Version v${packageInfo.version}"`
+	]);
+
+	await runAsync("git", ["tag", `v${packageInfo.version}`]);
+	await runAsync("yarn", ["publish", join(REPO_DIR, "packages", "eslint-config-c74"), "--new-version", packageInfo.version]);
+	await runAsync("yarn", ["publish", join(REPO_DIR, "packages", "eslint-config-c74-base"), "--new-version", packageInfo.version]);
+	await runAsync("yarn", ["publish", join(REPO_DIR, "packages", "eslint-config-c74-ts"), "--new-version", packageInfo.version]);
+});
