a0ef34ab13bd353c9da137591440799756a91989
Florian Demmer <florian@cycling74.com>
2023-09-19 14:15:08 +0100

    fixed version script

--- scripts/version.js
+++ scripts/version.js
@@ -1,9 +1,23 @@
 #!/usr/bin/env node
+const { join } = require("path");
 const { execSync } = require("child_process");
+const { readFileSync, writeFileSync } = require("fs");
 const { workspacePackages, REPO_ROOT } = require("./helpers");
 
 const version = process.env.npm_package_version;
 
-for (const [name] of Object.entries(workspacePackages)) {
-	execSync(`npm version ${version} --git-tag-version=false --workspace=${name}`, { cwd: REPO_ROOT });
+for (const [,path] of Object.entries(workspacePackages)) {;
+
+	const pkgInfoPath = join(REPO_ROOT, path, "package.json");
+	const pkgInfo = JSON.parse(readFileSync(pkgInfoPath));
+	pkgInfo.version = version;
+
+	for (const depName of Object.keys(workspacePackages)) {
+		if (pkgInfo.dependencies && pkgInfo.dependencies[depName]) pkgInfo.dependencies[depName] = version;
+		if (pkgInfo.devDependencies && pkgInfo.devDependencies[depName]) pkgInfo.devDependencies[depName] = version;
+	}
+
+	writeFileSync(pkgInfoPath, JSON.stringify(pkgInfo, null, "\t"));
+	execSync(`git add ${pkgInfoPath}`, { cwd: REPO_ROOT });
+
 }
