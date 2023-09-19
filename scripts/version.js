70d6139bd8823890f04c0ca0eea2e676e33f8b6d
Florian Demmer <florian@cycling74.com>
2023-09-19 14:17:07 +0100

    one more version script fix

--- scripts/version.js
+++ scripts/version.js
@@ -6,7 +6,7 @@ const { workspacePackages, REPO_ROOT } = require("./helpers");
 
 const version = process.env.npm_package_version;
 
-for (const [name, path] of Object.entries(workspacePackages)) {
+for (const path of Object.values(workspacePackages)) {
 
 	const pkgInfoPath = join(REPO_ROOT, path, "package.json");
 	const pkgInfo = JSON.parse(readFileSync(pkgInfoPath));
